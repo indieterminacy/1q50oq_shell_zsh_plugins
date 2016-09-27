03a61fa26cceea31b5aeee8a83542db5de263d68
indieterminacy <indieterminacy@nortonspeal.com>
2016-09-27 16:54:37 +0200

    Random commit, probably added list

---
+++ oqo_environment.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/zsh
+
+# zplug "tcnksm/docker-alias", of:zshrc
+
+
+# Switch in and out of command-line apps with one keystroke: Ctrl-Z
+# Not sure if functional
+zplug "Peeja/ctrl-zsh"
+# https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/fancy-ctrl-z
+
