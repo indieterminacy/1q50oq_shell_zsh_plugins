85c1bf7a4669a94dd0ee86a8ddf8d3c15f2da9ec
Indieterminacy <indieterminacy@nortonspeal.com>
2019-01-02 20:32:16 +0100

    Added plugins

--- oqo_environment.zsh
+++ oqo_environment.zsh
@@ -2,6 +2,13 @@
 
 # zplug "tcnksm/docker-alias", of:zshrc
 
+# Run a command after a plugin is installed/updated
+zplug "tj/n", \
+    hook-build:"make install"
+
+# Install if "if:" tag returns true
+# zplug "hchbaw/opp.zsh", if:"(( ${ZSH_VERSION%%.*} < 5 ))"
+
 
 # tet Switch in and out of command-line apps with one keystroke: Ctrl-Z
 # TODO Not sure if functional
